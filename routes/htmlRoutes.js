3a73d5afff6b683903888ae8fa70e600e919d207
Drock4440 <daltonrothrock8@gmail.com>
2021-07-18 19:56:54 -0600

    fixing htmlRoute

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,8 +7,7 @@ module.exports = function(app) {
     app.get('/notes', function(req, res){
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     })
-}
-// this sends the data from the notes file to the index.html file
-    app.get('/', function(req, res){
+    app.get('*', function(req, res){
         res.sendFile(path.join(__dirname, '../public/index.html'));
     })
+}
