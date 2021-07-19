63b9f494bca18ca65ac8d7f08b21e1dd3d455d7f
Drock4440 <daltonrothrock8@gmail.com>
2021-07-18 20:01:46 -0600

    fixing htmlRoute

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,13 +1,14 @@
-const express = require('express');
 const path = require('path');
-const app = express();
+const router = require('express').Router();
 
-// this tells the application to send the data over to the notes.html file
-module.exports = function(app) {
-    app.get('/notes', function(req, res){
-        res.sendFile(path.join(__dirname, '../public/notes.html'));
-    })
-    app.get('*', function(req, res){
-        res.sendFile(path.join(__dirname, '../public/index.html'));
-    })
-}
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+module.exports = router;
+
